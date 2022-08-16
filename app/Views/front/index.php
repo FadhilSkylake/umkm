7bb55dc7c5928f42fe4308759421fde95af397aa
FadhilSkylake <fadhil1023@gmail.com>
2022-08-16 17:56:44 +0700

    commit

--- app/Views/front/index.php
+++ app/Views/front/index.php
@@ -77,20 +77,21 @@
         </div>
         <div class="row g-4 portfolio-container">
             <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
-                <div class="rounded overflow-hidden">
-
-                    <div class="position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="<?= base_url(); ?>/front/img/portfolio-1.jpg" alt="">
-                        <div class="portfolio-overlay">
-                            <a class="btn btn-square btn-outline-light mx-1" href="img/portfolio-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-square btn-outline-light mx-1" href=""><i class="fa fa-link"></i></a>
+                <?php foreach ($kegiatan as $k) : ?>
+                    <div class="rounded overflow-hidden">
+                        <div class="position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="/img/<?= $k['dokumentasi']; ?>" alt="">
+                            <div class="portfolio-overlay">
+                                <a class="btn btn-square btn-outline-light mx-1" href="/img/<?= $k['dokumentasi']; ?>" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                                <a class="btn btn-square btn-outline-light mx-1" href=""><i class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="border border-5 border-light border-top-0 p-4">
+                            <p class="text-primary fw-medium mb-2"><?= $k['nama_kegiatan']; ?></p>
+                            <h5 class="lh-base mb-0"><?= $k['deskripsi']; ?></a>
                         </div>
                     </div>
-                    <div class="border border-5 border-light border-top-0 p-4">
-                        <p class="text-primary fw-medium mb-2">General Carpentry</p>
-                        <h5 class="lh-base mb-0">Wooden Furniture Manufacturing And Remodeling</a>
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
